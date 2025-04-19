4b6278c6dd8ffbf4fda4d02b437b830c1711770a
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2025-04-19 16:08:46 +0530

    utils, async handler, apiError, apiResponse, app.js cors-json-cookie_parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:"./env"
-})
-connectDB();
+  path: "./env",
+  
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`SERVER IS RUNNING AT PORT NUMBER ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION FAILED !!!!", error);
+  });
 
 /*
 ************** BASIC DATABASE CONNECTION ANS SERVER SETUP **************
