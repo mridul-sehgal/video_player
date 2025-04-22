04c9966d162680977df121eafd2c793358c98e47
Mridul_Sehgal <mridul.sehgal1114@gmail.com>
2025-04-22 22:32:11 +0530

    set up multer  middleware for file upload in routes, create the register user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // 1) get user details from frontend,
+  const { username, fullname, email, password } = req.body;
+
+  // 2) perform validation on feilds
+  if (
+    [fullname, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All feilds are required");
+  }
+
+  // 3) checking if user already exists
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+
+  if (existedUser) {
+    throw new ApiError(409, "user with email or username already exists");
+  }
+
+  // 4) checking if files are avilable or not
+  const avatarLocalPath = body.files?.avatar[0]?.path;
+  const coverImageLocalPath = body.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  // 5) upload images to cloudinary
+  const avatar = uploadOnCloudinary(avatarLocalPath);
+  const coverImage = uploadOnCloudinary(coverImageLocalPath);
+
+  // 6) check if images are properly uploaded to cloudinary
+  if (!avatar) {
+    throw new ApiError(400, "Avatar is required");
+  }
+
+  // 7) create user object - create entry in db
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  //8) check for user creation and remove password and refresh token fields from response
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+
+  // 9) return response.
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
